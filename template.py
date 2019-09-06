76964c8ef5e2a6c01cc754be076c22c3b93fd8ef
Stefan Tauner <stauner@ecs.tuwien.ac.at>
2019-09-06 14:31:01 +0200

    template: put evaluation loop into endless loop in preparation for ftLTL

--- template.py
+++ template.py
@@ -20,31 +20,38 @@ def solve(trace, term_cnt):
   pre = [None]*term_cnt
   now = [None]*term_cnt
 
-  # Initialization
-  state = trace[0]
   print_array(trace, "trace")
-  vprint()
 
-  # The next statement will be replaced by expressions to initialize pre
-  template_init="template_init"
-
-  if pre[0] == 0:
-    return [1, 0] # Fail immediately if we start in a bad state
-
-  # Event interpretation loop
   iteration = 1
-  for i, state in enumerate(trace[1:], start=1):
-    print_state(i, state)
+  while True:
+    vprint("\nRunning ptLTL init #%d..." % iteration)
+    state = trace[0]
+    print_state(0, state)
 
-    # The next statement will be replaced by expressions to update now according to state
-    template_loop="template_loop"
+    # The next statement will be replaced by expressions to initialize state 0 under ptLTL interpretation
+    template_init="template_init"
 
-    if now[0] == 0:
-      return [1, i] # Fail immediately w/o taking further states into account
-
-    pre = now.copy()
+    if pre[0] == 0:
+      return [1, 0] # Fail immediately if we start in a bad state
 
     print_array_rev(pre, "pre")
     print_array_rev(now, "now")
 
-  return [0]
+    # ptLTL interpretation loop
+    vprint("\nRunning ptLTL loop interpretation #%d..." % iteration)
+    for i, state in enumerate(trace[1:], start=1):
+      print_state(i, state)
+
+      # The next statement will be replaced by expressions to update states 1... under ptLTL interpretation
+      template_loop="template_loop"
+
+      print_array_rev(pre, "pre")
+      print_array_rev(now, "now")
+      if now[0] == 0:
+        return [1, i] # Fail immediately w/o taking further states into account
+
+      pre = now.copy()
+      vprint()
+
+    return [0]
+
