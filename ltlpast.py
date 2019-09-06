e921b445b00ed0ed62a50baee70b9c2c926faf1f
Stefan Tauner <stauner@ecs.tuwien.ac.at>
2019-09-06 03:16:28 +0200

    ltlpast: get rid of manual AST coding

--- ltlpast.py
+++ ltlpast.py
@@ -8,21 +8,12 @@ from debug import *
 ####################
 
 # S_PREV, W_PREV a: "now[i] = pre[a]"
-def prev(target, i, args):
-  return ast.Subscript(
-      value=ast.Name(id="pre", ctx=ast.Load()),
-      slice=ast.Index(value=ast.Num(n=args[0]),),
-      ctx=ast.Load(),
-    )
+def prev(target, term, term_i, args):
+  return ast.parse("pre[{a}]".format(a=args[0]))
 
-# Simply set target[i] = a (target(args[0]))
-def init_target(target, i, args):
-  return ast.Subscript(
-      value=ast.Name(id=target, ctx=ast.Load()),
-      slice=ast.Index(value=ast.Num(n=args[0]),),
-      ctx=ast.Load(),
-    )
-
+# Simply set target[i] = target[a]
+def init_target(target, term, term_i, args):
+  return ast.parse("{t}[{a}]".format(t=target, a=args[0]))
 
 ######################################
 # Hashmap of initialization routines #
@@ -32,42 +23,37 @@ init_dict = {
   # Nullary operators (atoms, truth values)                                                         #
   ###################################################################################################
   # boolean: target[i] = b
-  'bool': lambda target, i, b:
-    ast.NameConstant(value=b)
-  ,
+  'bool': lambda target, term, term_i, b:
+    ast.parse("{b}".format(b=b)),
 
   # $atom: target[i] = state['atom']
-  'atom': lambda target, i, atom:
-    ast.Subscript(value=ast.Name(id='state', ctx=ast.Load()), slice=ast.Index(value=ast.Str(s=atom)), ctx=ast.Load())
-  ,
+  'atom': lambda target, term, term_i, atom:
+    ast.parse("state['{atom}']".format(atom=atom)),
 
   ###################################################################################################
   # Unary operators ('NOT','S_PREV','W_PREV','S_NEXT','W_NEXT','ONCE','HIST','EVENTUALLY','ALWAYS') #
   # To initialize pre[i] we simply need to take into account pre[args[0]] (args[1] is ignored)      #
   ###################################################################################################
   # NOT: "target[i] = not target[args[0]]"
-  'NOT': lambda target, i, args:
-    ast.UnaryOp(
-                  op=ast.Not(),
-                  operand=ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[0]),),
-                      ctx=ast.Load(),
-                  ),
-                )
-  ,
+  'NOT': lambda target, term, term_i, args:
+    ast.parse("not {0}[{1[0]}] if {0}[{1[0]}] is not None else None".format(target, args)),
 
   # S_PREV: "target[i] = 0"
-  'S_PREV': lambda target, i, args:
-    ast.NameConstant(value=False)
-  ,
+  'S_PREV': lambda target, term, term_i, args:
+    ast.parse("False"),
 
   # W_PREV: "target[i] = a" - This is actually the only difference to S_PREV
   'W_PREV': init_target,
+
   # ONCE a: "target[i] = a"
   'ONCE': init_target,
+
   # HIST a: "target[i] = a"
   'HIST': init_target,
+
+  # W_NEXT a: "target[i] = None"
+  'W_NEXT': lambda target, term, term_i, args:
+    ast.parse("None"),
 
   #######################################################
   # Binary operators ('OR','AND','IMP','SINCE','UNTIL') #
@@ -77,73 +63,20 @@ init_dict = {
   # We assume here to simply get the two respective indices as inputs.
   
   # $a OR $b: "target[i] = target[a] or target[b]"
-  'OR': lambda target, i, args:
-    ast.BoolOp(
-                  op=ast.Or(),
-                  values=[
-                    ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[0]),),
-                      ctx=ast.Load(),
-                    ),
-                    ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[1]),),
-                      ctx=ast.Load(),
-                    ),
-                  ],
-                )
-  ,
+  'OR': lambda target, term, term_i, args:
+    ast.parse("{0}[{a}] or {0}[{b}] if None not in [{0}[{a}],{0}[{b}]] else None".format(target, a=args[0], b=args[1])),
 
   # $a AND $b: "target[i] = target[a] and target[b]"
-  'AND': lambda target, i, args:
-    ast.BoolOp(
-                  op=ast.And(),
-                  values=[
-                    ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[0]),),
-                      ctx=ast.Load(),
-                    ),
-                    ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[1]),),
-                      ctx=ast.Load(),
-                    ),
-                  ],
-                )
-  ,
+  'AND': lambda target, term, term_i, args:
+    ast.parse("{0}[{a}] and {0}[{b}] if None not in [{0}[{a}],{0}[{b}]] else None".format(target, a=args[0], b=args[1])),
 
   # $a IMP $b: "target[i] = (not target[a]) or target[b]"
-  'IMP': lambda target, i, args:
-    ast.BoolOp(
-                  op=ast.Or(),
-                  values=[
-                    ast.UnaryOp(
-                      op=ast.Not(),
-                      operand=ast.Subscript(
-                        value=ast.Name(id=target, ctx=ast.Load()),
-                        slice=ast.Index(value=ast.Num(n=args[0]),),
-                        ctx=ast.Load(),
-                      ),
-                    ),
-                    ast.Subscript(
-                      value=ast.Name(id=target, ctx=ast.Load()),
-                      slice=ast.Index(value=ast.Num(n=args[1]),),
-                      ctx=ast.Load(),
-                    ),
-                  ],
-                )
-  ,
+  'IMP': lambda target, term, term_i, args:
+    ast.parse("not {0}[{a}] or {0}[{b}] if None not in [{0}[{a}],{0}[{b}]] else None".format(target, a=args[0], b=args[1])),
 
   # $a SINCE $b: "target[i] = target[b]"
-  'SINCE': lambda target, i, args:
-    ast.Subscript(
-          value=ast.Name(id=target, ctx=ast.Load()),
-          slice=ast.Index(value=ast.Num(n=args[1]),),
-          ctx=ast.Load(),
-      )
-  ,
+  'SINCE': lambda target, term, term_i, args:
+    ast.parse("{0}[{b}]".format(target, b=args[1])),
 }
 
 ###############################
@@ -161,71 +94,16 @@ loop_dict = {
   'W_PREV': prev,
 
   # $a SINCE $b: "target[i] = target[b] or (target[a] and pre[i])"
-  'SINCE': lambda target, i, args:
-      ast.BoolOp(
-        op=ast.Or(),
-        values=[
-          ast.Subscript(
-            value=ast.Name(id=target, ctx=ast.Load()),
-            slice=ast.Index(value=ast.Num(n=args[0])),
-            ctx=ast.Load(),
-          ),
-          ast.BoolOp(
-            op=ast.And(),
-            values=[
-              ast.Subscript(
-                value=ast.Name(id=target, ctx=ast.Load()),
-                slice=ast.Index(value=ast.Num(n=args[0]),),
-                ctx=ast.Load(),
-              ),
-              ast.Subscript(
-                value=ast.Name(id='pre', ctx=ast.Load()),
-                slice=ast.Index(value=ast.Num(n=i)),
-                ctx=ast.Load(),
-              ),
-            ],
-          ),
-        ],
-      )
-  ,
+  'SINCE': lambda target, term, term_i, args:
+    ast.parse("{t}[{b}] or ({t}[{a}] and pre[{i}]) if None not in [{t}[{a}],{t}[{b}],pre[{i}]] else None".format(t=target, a=args[0], b=args[1], i=term_i)),
 
   # ONCE a: "target[i] = pre[i] or target[a]"
-  'ONCE': lambda target, i, args:
-      ast.BoolOp(
-        op=ast.Or(),
-        values=[
-          ast.Subscript(
-            value=ast.Name(id="pre", ctx=ast.Load()),
-            slice=ast.Index(value=ast.Num(n=i),),
-            ctx=ast.Load(),
-          ),
-          ast.Subscript(
-            value=ast.Name(id=target, ctx=ast.Load()),
-            slice=ast.Index(value=ast.Num(n=args[0]),),
-            ctx=ast.Load(),
-          ),
-        ],
-      )
-  ,
+  'ONCE': lambda target, term, term_i, args:
+    ast.parse("pre[{i}] or {t}[{a}] if None not in [{t}[{a}],pre[{i}]] else None".format(i=term_i, t=target, a=args[0])),
 
   # HIST a: "target[i] = pre[i] and target[a]"
-  'HIST': lambda target, i, args:
-      ast.BoolOp(
-        op=ast.And(),
-        values=[
-          ast.Subscript(
-            value=ast.Name(id="pre", ctx=ast.Load()),
-            slice=ast.Index(value=ast.Num(n=i),),
-            ctx=ast.Load(),
-          ),
-          ast.Subscript(
-            value=ast.Name(id=target, ctx=ast.Load()),
-            slice=ast.Index(value=ast.Num(n=args[0]),),
-            ctx=ast.Load(),
-          ),
-        ],
-      )
-  ,
+  'HIST': lambda target, term, term_i, args:
+    ast.parse("pre[{i}] and {t}[{a}] if None not in [{t}[{a}],pre[{i}]] else None".format(i=term_i, t=target, a=args[0])),
 }
 
 def generate_solver(terms, atoms):
@@ -264,7 +142,7 @@ class transformer(ast.NodeTransformer):
       term_idx = self.term_cnt-1-i
       if isinstance(t, str):
         vprint("%s[%d] = state[%s]" % (target, term_idx, t))
-        newvalue = op_dict['atom'](target, term_idx, t)
+        newvalue = op_dict['atom'](target, t, term_idx, t)
       elif isinstance(t, tuple):
         op = t[0]
         try:
@@ -276,16 +154,17 @@ class transformer(ast.NodeTransformer):
         a = term_idx + 1
         b = term_idx + 1 + tt[1]
         vprint("%s[%d] = %s(..., %s[%d], %s[%d])" % (target, term_idx, op, target, a, target, b))
-        newvalue = assign(target, term_idx, [a, b])
+        newvalue = assign(target, t, term_idx, [a, b])
       elif isinstance(t, bool):
         vprint("%s[%d] = %s" % (target, term_idx, str(t)))
-        newvalue = op_dict['bool'](target, term_idx, t)
+        newvalue = op_dict['bool'](target, t, term_idx, t)
       else:
         raise Exception("Unknown term type at %d: %s (%s)" % (i, type(t), t))
 
       newnode = ast.Assign(
         targets=[ast.Subscript(value=ast.Name(id=target, ctx=ast.Load()), slice=ast.Index(value=ast.Num(n=term_idx)), ctx=ast.Store())],
-        value=newvalue
+        # The parsed ASTs contain a whole module that we need to unwrap
+        value=newvalue.body[0].value
       )
       newnode.lineno = node.lineno
       newnode.col_offset = node.col_offset
